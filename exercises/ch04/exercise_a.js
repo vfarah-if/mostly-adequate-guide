4900059cdc0d7a23df201b39246611277383b071
vfarah-if <sefrica@live.com>
2019-04-02 11:52:48 +0100

    Chapter 04 exercises and notes

--- exercises/ch04/exercise_a.js
+++ exercises/ch04/exercise_a.js
@@ -1,4 +1,5 @@
 // Refactor to remove all arguments by partially applying the function.
 
 // words :: String -> [String]
+const split = (delimiter, str) => str.split(delimiter);
 const words = str => split(' ', str);
